023c9877f3fe1575693a9fe25084abb3dafb9b5d
violette-sanchez <violettes@nchez.fr>
2026-04-13 12:54:50 +0200

    vite deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
-import {defineConfig, loadEnv} from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig(({mode}) => {
+export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [tailwindcss()],
